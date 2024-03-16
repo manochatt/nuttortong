c2bde7d403af029cdd497c3ced7e4daedf85044d
Thanatat Srisung <thanatat.sr@gmail.com>
2024-03-16 22:15:40 +0700

    Edit Mongo module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://thanatatsr:eNkeT4RPQ2RwXMDR@cluster0.gllk4x3.mongodb.net/store',
-    ),
-    ProductsModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URL), ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
